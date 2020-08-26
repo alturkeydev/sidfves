873f4e88a2360d2bc122f6cd9308d149bec37820
Younes <alturkeyy@gmail.com>
2020-08-26 21:24:39 +0300

    latest edits

--- src/Components/Calculator/Calculator.js
+++ src/Components/Calculator/Calculator.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Calculator.css'
 
 
 class Calculator extends React.Component {
@@ -18,29 +19,30 @@ class Calculator extends React.Component {
   render() {
     return(
       <div className="pt5">
-        <div className='br3 white k-10 mv3 w-70 w-70-m w-50-l mw7 shadow-1 center'>
-          <h1 style={{fontFamily: 'Courier New'}}>Irfan Check Quick-Tool</h1>
+        <div className='Pattern br3 pl5 black k-10 mv3 w-70 w-70-m w-50-l mw7 shadow-1 center'>
+          <h1 style={{fontFamily: 'Courier New'}}>Irfan Award Quick-Tool<p className="f6">This automated tool generates an estimate figure of the final Irfan Award.</p></h1>
+          <div></div>
         </div>
-        <article className='br3 white k-10 mv3 w-70 w-70-m w-50-l mw7 shadow-1 center'>
-          <main className="pa4 white-80">
+        <article className='Pattern br3 black k-10 mv3 w-70 w-70-m w-50-l mw7 shadow-1 center'>
+          <main className="pa4 black-80">
             <div className="measure">
               <fieldset id="input" className="ba b--transparent ph0 mh0">
                 <div className="mt3">
-                  <label style={{fontFamily: 'Courier New'}} className="db fw6 lh-copy f6 white b">Basic Salary</label>
+                  <label style={{fontFamily: 'Courier New'}} className="db fw6 lh-copy f6 black b">Basic Salary</label>
                   <input
                     style={{fontFamily: 'Courier New'}}
                     onChange={this.onBSalaryChange}
                     placeholder="13500"
-                    className="pa2 b white ba bg-transparent hover-bg-black hover-white w-100 ba b--near-white"
+                    className="pa2 b black ba bg-transparent hover-bg-black hover-black w-100 ba b--near-black"
                     type="text"/>
                   </div>
                 <div className="mv3">
-                  <label className="db fw6 lh-copy f6 white b" style={{fontFamily: 'Courier New'}}>Date of Birth</label>
+                  <label className="db fw6 lh-copy f6 black b" style={{fontFamily: 'Courier New'}}>Date of Birth</label>
                   <input
                     style={{fontFamily: 'Courier New'}}
                     onChange={this.onDOBChange}
                     placeholder="1995-10-26"
-                    className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 ba b--near-white"
+                    className="b pa2 input-reset ba bg-transparent hover-bg-black hover-black w-100 ba b--near-black"
                     type="date"
                     name="date"
                     id="date-of-birth"/>
